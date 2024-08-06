001e9c72871559890da926e94d21e6efb6e32ebd
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-08-06 09:12:43 +0700

    Add day 11 first react

---
+++ Day-11/src/app.js
@@ -0,0 +1,11 @@
+import Header from "./header";
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <h1>React</h1>
+            <p>This is react</p>
+        </div>
+    );
+}
