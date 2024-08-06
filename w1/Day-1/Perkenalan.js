362778dfeff2f42678eb76889668df7a7753fbd5
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-08-06 08:55:43 +0700

    :sparkles: add day 1

---
+++ w1/Day-1/Perkenalan.js
@@ -0,0 +1,10 @@
+let nama = 'Fathul'
+let age = '23'
+let domicile = 'Cibiru, Bandung'
+let hobbyList = ['Musik', 'Berkegiatan di alam bebas']
+let hobby = hobbyList.toString();
+
+let text =
+    `Perkenalkan nama saya ${nama}. Umur saya saat ini ${age} tahun. Saya berdomisili di ${domicile}. Hobi saya diantaranya ${hobby}`
+
+console.info(text);
