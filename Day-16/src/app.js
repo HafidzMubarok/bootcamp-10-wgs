863e91c556186dba72d04fe72e4eaf67af681a4d
HafidzMubarok <mubarokhafidz29@gmail.com>
2024-08-12 13:49:06 +0700

    :sparkles: Add videos menu and move comment to home

---
+++ Day-16/src/app.js
@@ -0,0 +1,34 @@
+import Header from "./header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Comment from "./components/Comment";
+import Gallery from "./components/Gallery";
+import Videos from "./components/Videos";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function NotFound() {
+    return <h1>Page not found</h1>;
+}
+
+export default function App() {
+
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                <div style={{ paddingLeft: "64px", paddingTop: "32px", paddingBottom: "64px" }}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="about" element={<About />} />
+                        <Route path="contact" element={<Contact />} />
+                        {/* <Route path="comment" element={<Comment />} /> */}
+                        <Route path="gallery" element={<Gallery />} />
+                        <Route path="videos" element={<Videos />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+}
---
+++ Day-16/src/header.js
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import LiveTime from "./components/LiveTime";
+
+import "./header.css"
+
+export default function Header() {
+    const title = "Experiment with REACTJS";
+
+    return (
+        <>
+            <nav>
+                <h1>{title}</h1>
+                <div>
+                    <i aria-hidden="true" className="clock icon"></i>
+                    <LiveTime />
+                </div>
+                <ul style={{ display:"flex", flexDirection: "row", listStyleType: "none", }}>
+                    <li>
+                        <Link to="/">home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">about</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/gallery">gallery</Link>
+                    </li>
+                    {/* <li>
+                        <Link to="/comment">comment</Link>
+                    </li> */}
+                    <li>
+                        <Link to="/videos">videos</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+}
